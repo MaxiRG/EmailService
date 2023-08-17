eb697c3bb2f62777630c198d59bd128d9638294b
MaxiRG <maxiramirezgis@gmail.com>
2023-08-17 12:37:01 -0300

    Emails are saved in db.

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -2,13 +2,14 @@ import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { EmailService } from "./email.service";
 import { EmailDto } from "./dto";
+import { GetUser } from "../auth/decorator";
 
 @Controller("email")
 export class EmailController {
   constructor(private emailService: EmailService) {}
   @UseGuards(AuthGuard("jwt"))
   @Post("send-email")
-  sendEmail(@Body() dto: EmailDto) {
-    return this.emailService.sendEmail(dto);
+  sendEmail(@Body() dto: EmailDto, @GetUser("id") userId: number) {
+    return this.emailService.sendEmail(dto, userId);
   }
 }
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from "@nestjs/common";
 import { EmailDto } from "./dto";
 import { ConfigService } from "@nestjs/config";
+import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
 export class EmailService {
-  constructor(private config: ConfigService) {}
-  sendEmail(dto: EmailDto) {
+  constructor(
+    private config: ConfigService,
+    private prisma: PrismaService,
+  ) {}
+  async sendEmail(dto: EmailDto, userId: number) {
     this.sendGridProvider(dto);
-    return "Email sent!";
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+    const email = await this.prisma.email.create({
+      data: {
+        userId: user.id,
+      },
+    });
+    return email;
   }
   sendGridProvider(dto: EmailDto) {
     const sgMail = require("@sendgrid/mail");
